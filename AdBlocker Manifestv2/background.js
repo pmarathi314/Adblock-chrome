f011b9353e8f92cfed4377bc7c34c7ddb1e4a560
pmarathi314 <44821024+pmarathi314@users.noreply.github.com>
2022-05-24 20:25:46 -0500

    Add files via upload
    bare-bones working adblocker on manifest v2

---
+++ AdBlocker Manifestv2/background.js
@@ -0,0 +1,19 @@
+//array of blackListed websites
+const blackListed = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return {cancel: true}; },
+    { urls: blackListed },
+    ["blocking"]
+    );
